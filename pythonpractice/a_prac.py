c8c2e4472d13200847b3a34b1c9d2b6f8c20b62e
Sumedh Ghule <sumedh.ghule123@gmail.com>
2025-06-09 15:14:15 +0530

    rock paper scissors game and rolling dice

---
+++ pythonpractice/a_prac.py
@@ -0,0 +1,26 @@
+# To practice all previous days on sunday!!
+
+# The username must start with a letter, be under 10 chars, and contain no digits.
+
+# username = input("Enter your username: ")
+
+
+# while True:
+#     username = input("Enter your username: ")
+#     if username[0].isalpha():
+#         if username.isalpha()== False:
+#             print("Username must not contain any digits")
+#         elif len(username)>= 10:
+#             print("Username must be less than 10 characters")
+#         else:
+#             print(f"Your username is {username}")
+#             break
+#     else:
+#         # print("Your username must start with a letter")
+
+for i in range(3):
+    for j in range(2):
+        print(f"({i},{j})", end=" ")
+# output - first row -(1,1) (1,2)
+#         second row - (2,1) (2,2)
+#         thir row  - (3,1) (3,2)
--- pythonpractice/dump.py
+++ pythonpractice/dump.py
@@ -106,37 +106,134 @@
 
 
 
-#num guessing game
+# #num guessing game
 
-# Python number guessing game
+# # Python number guessing game
+
+# import random
+# lowest_num = 1
+# highest_num = 100
+# answer = random.randint(lowest_num, highest_num)
+# guesses = 0
+# is_running = True
+# print("Python Number Guessing Game")
+# print(f"Select a number between {lowest_num} and {highest_num}")
+# while is_running:
+
+#     guess = input("Enter your guess: ")
+
+#     if guess.isdigit():
+#         guess = int(guess)
+#         guesses += 1
+#         if guess < lowest_num or guess > highest_num:
+#             print("That number is out of range")
+#             print(f"Please select a number between {lowest_num} and {highest_num}")
+
+#         elif guess < answer:
+#             print("Too low! Try again!")
+#         elif guess > answer:
+#             print("Too high! Try again!")
+#         else:
+#             print(f"CORRECT! The answer was {answer}")
+#             print(f"Number of guesses: {guesses}")
+#             is_running = False
+#     else:
+#         print("Invalid guess")
+#         print(f"Please select a number between {lowest_num} and {highest_num}")
+
+
+
+'''
+
+'''
+
+
+# import random
+# options = ("rock", "paper", "scissors")
+# running = True
+# while running:
+#    player = None
+#    computer = random.choice(options)
+#    while player not in options:
+#        player = input("Enter a choice (rock, paper, scissors): ")
+#        print(f"DEBUG: player='{player}', in options? {player in options}")
+#    print(f"Player: {player}")
+#    print(f"Computer: {computer}")
+#    if player == computer:
+#        print("It's a tie!")
+#    elif player == "rock" and computer == "scissors":
+#        print("You win!")
+#    elif player == "paper" and computer == "rock":
+#        print("You win!")
+#    elif player == "scissors" and computer == "paper":
+#        print("You win!")
+#    else:
+#        print("You lose!")
+#    if not input("Play again? (y/n): ").lower() == "y":
+#        running = False
+# print("Thanks for playing!")
+
+
+
+
+'''
+
+'''
+
 
 import random
-lowest_num = 1
-highest_num = 100
-answer = random.randint(lowest_num, highest_num)
-guesses = 0
-is_running = True
-print("Python Number Guessing Game")
-print(f"Select a number between {lowest_num} and {highest_num}")
-while is_running:
 
-    guess = input("Enter your guess: ")
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
 
-    if guess.isdigit():
-        guess = int(guess)
-        guesses += 1
-        if guess < lowest_num or guess > highest_num:
-            print("That number is out of range")
-            print(f"Please select a number between {lowest_num} and {highest_num}")
+dice = []
+total = 0
+num_of_dice = int(input("How many dice?: "))
 
-        elif guess < answer:
-            print("Too low! Try again!")
-        elif guess > answer:
-            print("Too high! Try again!")
-        else:
-            print(f"CORRECT! The answer was {answer}")
-            print(f"Number of guesses: {guesses}")
-            is_running = False
-    else:
-        print("Invalid guess")
-        print(f"Please select a number between {lowest_num} and {highest_num}")
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+# # PRINT VERTICALLY
+# for die in range(num_of_dice):
+# for line in dice_art.get(dice[die]):
+# print(line)
+
+# PRINT HORIZONTALLY
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+
+for die in dice:
+    total += die
+print(f"total: {total}")
